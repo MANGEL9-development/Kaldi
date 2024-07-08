fc14bcd2fec31ae470964c0870bac2efd6a970a5
MANGEL9-development <99140478+MANGEL9-development@users.noreply.github.com>
2024-07-07 20:47:29 -0400

    Commit 2024.07.07
    - Styled the menu bar's search component
    
    Other minor fixes include
       - Added a variable standard-transition-time in style-main.scss as 0.2s
       - Removed #searchbar from #search-container in searchBar() in menubar.js

--- src/components/pieces/menubar.js
+++ src/components/pieces/menubar.js
@@ -2,16 +2,15 @@ import sitemap from "../sitemap.json"
 
 function searchBar(){
     return (
-        <div id="search-container">
-            <form id="searchbar" class="">
-                <div id="search-input-container">
-                    <input type="search" name="searchquery" id="search-input" placeholder="What are you craving?" />
-                    <button type="submit">
-                        <div id="search-button-onhover"></div>
-                    </button>
-                </div>
-            </form>
-        </div>
+        <form id="searchbar" class="">
+            <div id="search-input-container">
+                <input type="search" name="searchquery" id="search-input" placeholder="What are you craving?"/>
+                {/* TODO: add .focused to #search-input-container when input is focused. remove on blur */}
+                <button type="submit">
+                    <div id="search-button-onhover"></div>
+                </button>
+            </div>
+        </form>
     );
 }
 
